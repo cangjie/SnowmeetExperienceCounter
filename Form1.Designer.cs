d7cb757466eecb0b10c3e1b6f5ff121c3f06ca80
cangjiie <cangjie@hotmail.com>
2021-11-10 19:26:52 +0800

    display web image

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,14 +33,17 @@ namespace SnowmeetExperienceCounter
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.listView1 = new System.Windows.Forms.ListView();
             this.imageList = new System.Windows.Forms.ImageList(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // statusStrip1
             // 
             this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
-            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 1130);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(0, 0, 20, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(2494, 22);
             this.statusStrip1.TabIndex = 0;
             this.statusStrip1.Text = "statusStrip1";
             this.statusStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.statusStrip1_ItemClicked);
@@ -48,9 +51,10 @@ namespace SnowmeetExperienceCounter
             // listView1
             // 
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(33, 22);
+            this.listView1.Location = new System.Drawing.Point(40, 26);
+            this.listView1.Margin = new System.Windows.Forms.Padding(4);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(742, 382);
+            this.listView1.Size = new System.Drawing.Size(1516, 623);
             this.listView1.TabIndex = 1;
             this.listView1.UseCompatibleStateImageBehavior = false;
             // 
@@ -60,22 +64,42 @@ namespace SnowmeetExperienceCounter
             this.imageList.ImageSize = new System.Drawing.Size(16, 16);
             this.imageList.TransparentColor = System.Drawing.Color.Transparent;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(140, 781);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(329, 127);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "删   除";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(799, 781);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(329, 127);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "结    算";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.ControlBox = false;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(2494, 1152);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.statusStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
-            this.ShowInTaskbar = false;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -86,6 +110,8 @@ namespace SnowmeetExperienceCounter
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.ImageList imageList;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,18 +7,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.IO;
 
 namespace SnowmeetExperienceCounter
 {
     public partial class Form1 : Form
     {
+        public string[] productImgUrlArr = new string[] { "https://img.alicdn.com/bao/uploaded/i1/3040100879/O1CN016bSDc91IMciITcaVP_!!3040100879.jpg",
+            "https://img.alicdn.com/bao/uploaded/i2/3040100879/TB2oOX6ac2DjeFjSspnXXb20XXa_!!3040100879.jpg",
+            "https://img.alicdn.com/bao/uploaded/i1/3040100879/TB1jOjZSFXXXXXJXVXXXXXXXXXX_!!0-item_pic.jpg",
+            "https://img.alicdn.com/bao/uploaded/i2/3040100879/O1CN01ZYjDby1IMcik8huBY_!!3040100879.jpg" };
+
         public Form1()
         {
             InitializeComponent();
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create("https://img.alicdn.com/bao/uploaded/i1/3040100879/O1CN016bSDc91IMciITcaVP_!!3040100879.jpg");
+            HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+            Stream s = res.GetResponseStream();
+            Image img = Image.FromStream(s);
+            s.Close();
+            res.Close();
+            req.Abort();
+            imageList.Images.Add(img);
+            imageList.ImageSize = new Size(30, 40);
+            listView1.LargeImageList = imageList;
+            ListViewItem item = new ListViewItem("test");
+            item.ImageIndex = 0;
+            listView1.Items.Add(item);
+            
             
         }
 
         private void statusStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
